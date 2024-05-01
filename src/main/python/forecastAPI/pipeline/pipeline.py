967fea38600516c19287b1b69302ed189b31c30e
Roy_Xu <ruoyu.xv@gmail.com>
2024-05-01 18:53:56 +0200

    Complete forecast pipeline

--- src/main/python/forecastAPI/pipeline/pipeline.py
+++ src/main/python/forecastAPI/pipeline/pipeline.py
@@ -20,8 +20,53 @@ def _process_point_metadata(json: dict) -> dict:
     meta['forecastGridData'] = json['properties']['forecastGridData']
     return meta
 
-def forecast_pipeline(json: dict):
+def _raw_forcast_dataframe(json) -> pd.DataFrame:
+    raw_data = json['properties']['periods']
+    normalized = pd.json_normalize(raw_data)
+    df = pd.DataFrame(normalized)
+    return df
+
+def _parse_time(data: pd.DataFrame, column: str):
+    """Parse a column of time data into 2 columns.
+
+    Args:
+        data (DataFrame): DataFrame with column to be parsed.
+        column (str): The column of time data of format
+            {YYYY-MM-DD}T{hh:mm:ss}-{mm:ss}.
+
+    Returns:
+        parsed_data (DataFrame): A DataFrame of 2 columns:
+            - 'date': YYYY-MM-DD.
+            - 'time': hh:mm
+    
+        For example, '2024-04-30T08:00:00-04:00' will be parsed into:
+            - 'date': '2024-04-30'
+            - 'time': '08:00'
+    """
+    parsed_data = pd.DataFrame()
+    parsed_data[['date', 'time']] = data['startTime'].str.split('T', expand=True)
+    parsed_data['time'] = parsed_data['time'].apply(lambda time: time[:5])
+    return parsed_data
+
+def forecast_pipeline(json: dict) -> pd.DataFrame:
     '''Process forecast data'''
-    raw_data = json['properties']
-    df = pd.DataFrame(raw_data)
+    df = _raw_forcast_dataframe(json)
+
+    # Change Fahrenheit to Celsius
+    df['temperature'] = round(5 * (df['temperature'] - 35) / 9)
+    df['temperature'] = df['temperature'].astype('int64')
+
+    df[['date', 'time']] = _parse_time(df, 'startTime')
+
+    df.drop(columns=[
+        'startTime',
+        'endTime',
+        'temperatureUnit',
+        'probabilityOfPrecipitation.unitCode', 
+        'dewpoint.unitCode',
+        'relativeHumidity.unitCode',
+        'icon'
+        ],
+        inplace=True)
+
     return df
--- src/test/python/forecastAPI_test/pipeline_test/test_pipeline.py
+++ src/test/python/forecastAPI_test/pipeline_test/test_pipeline.py
@@ -2,7 +2,10 @@ import unittest
 import json
 import os
 
+import pandas.testing
+
 import pipeline
+import pipeline.pipeline
 
 class TestPipeline(unittest.TestCase):
 
@@ -11,9 +14,14 @@ class TestPipeline(unittest.TestCase):
         with open(test_file_path) as f:
             self.test_json = json.load(f)
 
+    def test_forecast_raw_dataframe(self):
+        df = pipeline.pipeline._raw_forcast_dataframe(self.test_json)
+        self.assertEqual(df.shape, (14, 19), msg='Shape of raw forecast dataframe incorrect.')
+
+
     def test_forecast_pipeline(self):
         result = pipeline.forecast_pipeline(self.test_json)
-        print(result)
+        self.assertEqual(result.shape, (14, 14), msg='Shape of processed forecast data incorrect.')
 
 if __name__ == '__main__':
     unittest.main()
